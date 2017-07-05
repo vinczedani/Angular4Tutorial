ef1c9c494bf9dbd21782b3f92873a850a54c4e11
Vincze Dani <vdani@codingsans.com>
2017-07-05 12:52:51 +0200

    Data bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+import { ServerComponent } from './server/server.component';
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
